4313e01a17829a43435afecd0b0b0ad486b706a8
Varthika Tunoori <vtunoori@gmail.com>
2025-04-21 15:45:30 -0400

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -2,17 +2,17 @@ import streamlit as st
 st.title("My First App")
 name = st.text_input("What's your name?")
 if name:
-st.write(f"Hello, Varthika!")
+  st.write(f"Hello, Varthika!")
 
 import openai as oa
 oa.title("My First App")
 name = oa.text_input("What's your name?")
 if name:
-oa.write(f"Hello, Varthika!")
+  oa.write(f"Hello, Varthika!")
 
 import numpy as n
 n.title("My First App")
 name = n.text_input("What's your name?")
 if name:
-n.write(f"Hello, Varthika!")
+  n.write(f"Hello, Varthika!")
 
