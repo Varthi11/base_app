6459c8bd469058c1d595ffd7fc79df5c46ba8d98
Varthika Tunoori <vtunoori@gmail.com>
2025-04-21 15:51:24 -0400

    Update double.py

--- double.py
+++ double.py
@@ -1,1 +1,11 @@
+import openai as oa
+oa.title("My First App")
+name = oa.text_input("What's your name?")
+if name:
+  oa.write(f"Hello, Varthika!")
 
+import numpy as n
+n.title("My First App")
+name = n.text_input("What's your name?")
+if name:
+  n.write(f"Hello, Varthika!")
