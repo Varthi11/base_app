b41a492e8c22bccdefbd2ef04b09b3eaf2100d03
Varthika Tunoori <vtunoori@gmail.com>
2025-04-21 15:51:17 -0400

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -4,15 +4,5 @@ name = st.text_input("What's your name?")
 if name:
   st.write(f"Hello, Varthika!")
 
-import openai as oa
-oa.title("My First App")
-name = oa.text_input("What's your name?")
-if name:
-  oa.write(f"Hello, Varthika!")
 
-import numpy as n
-n.title("My First App")
-name = n.text_input("What's your name?")
-if name:
-  n.write(f"Hello, Varthika!")
 
